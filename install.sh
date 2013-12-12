9ae123c9c5c88d0c6d2383520a0b0ee65f84e483
kitsunai <kitsunai@sanritz.co.jp>
2013-12-12 18:12:37 +0900

    noオプション忘れた

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install ruby2.0 chef
-sudo gem2.0 i bundler
+sudo gem2.0 i bundler --no-ri --no-rdoc
 bundle install --path=bundle
 bundle exec berks --path=cookbooks
 if [ "$1" = "oshigoto" ]; then
