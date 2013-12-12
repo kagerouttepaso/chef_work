fe865cc5cfedd6b8fd04bc5f0dd6824a18d30cdd
kitsunai <kitsunai@sanritz.co.jp>
2013-12-12 09:02:59 +0900

    base Recipeを配列使ってスマートにまとめた

--- site-cookbooks/base/recipes/default.rb
+++ site-cookbooks/base/recipes/default.rb
@@ -7,52 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
-package "tmux" do
-  action :install
+%w{tmux zsh vim-nox exuberant-ctags tree aptitude ruby2.0 ruby2.0-dev git subversion git-svn xubuntu-desktop}.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
 end
 
-package "zsh" do
-  action :install
-end
-
-package "vim-nox" do
-  action :install
-end
-
-package "exuberant-ctags" do
-  action :install
-end
-
-package "tree" do
-  action :install
-end
-
-package "aptitude" do
-  action :install
-end
-
-package "ruby2.0" do
-  action :install
-end
-
-package "ruby2.0-dev" do
-  action :install
-end
-
-package "git" do
-  action :install
-end
-
-package "subversion" do
-  action :install
-end
-
-package "git-svn" do
-  action :install
-end
-
-
-package "xubuntu-desktop" do
-  action :install
-end
