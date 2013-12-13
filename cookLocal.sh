828502d6ec53c7760e66dd948175c46aae674b03
kitsunai <goth.wasawasa@gmail.com>
2013-12-13 14:22:32 +0900

    インストールスクリプト修正

--- cookLocal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "$1" = "" ]; then
-    HostName="localhost"
-else
-    HostName=$1
-fi
-sudo chef-solo -c ./solo.rb -j ./nodes/${HostName}.json
--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+
+#Set up Hostname
+if [ "$1" = "" ]; then
+    HostName="localhost"
+else
+    HostName=$1
+fi
+
+#install package
 if [ ! -f "installed.lock" ]; then
     sudo apt-get install ruby ruby2.0 ruby2.0-dev
     sudo gem2.0 i bundler --no-ri --no-rdoc
 fi
+
 bundle install --path=bundle
 bundle exec berks --path=cookbooks
+
+#install chef
 if [ ! -f "installed.lock" ]; then
     bundle exec knife solo prepare localhost
     knife configre
     touch installed.lock
 fi
-./cookLocal.sh
+
+#cook
+sudo chef-solo -c ./solo.rb -j ./nodes/${HostName}.json
