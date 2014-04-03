4810cf18faaf4b39cfb0e797a4a91b351d194a65
kitsunai <kitsunai@sanritz.co.jp>
2014-04-03 11:06:22 +0900

    modify metadatas

--- site-cookbooks/pxe-server/CHANGELOG.md
+++ site-cookbooks/pxe-server/CHANGELOG.md
@@ -5,7 +5,7 @@ This file is used to list changes made in each version of the pxe-server cookboo
 
 0.1.0
 -----
-- [your_name] - Initial release of pxe-server
+- [kitsunai] - Initial release of pxe-server
 
 - - -
 Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
--- site-cookbooks/pxe-server/metadata.rb
+++ site-cookbooks/pxe-server/metadata.rb
@@ -1,7 +1,7 @@
 name             'pxe-server'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'sanritz automation'
+maintainer_email 'kitsunai@sanritz.co.jp'
+license          'MIT'
 description      'Installs/Configures pxe-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
