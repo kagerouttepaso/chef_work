0ec2550c5041e779274354b232723c99e48be061
kitsunai <goth.wasawasa@gmail.com>
2014-04-11 11:57:04 +0900

    rbenvのインストールをパッケージから `git clone` に変更

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo apt-get install openssh-server git
+if [ ! -d "~/.rbenv" ]; then
+    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
 
 #Set up Hostname
 if [ "$1" = "" ]; then
@@ -9,7 +16,6 @@ fi
 
 #install package
 if [ ! -f "installed.lock" ]; then
-    sudo apt-get install openssh-server rbenv ruby-build
     rbenv install 1.9.3-rc1
     rbenv local 1.9.3-rc1
     rbenv rehash
