41e6129b7eadbbfb3b4fa50c8f1b4e906ac12f0b
kitsunai <goth.wasawasa@gmail.com>
2014-04-21 23:14:58 +0900

    modify

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,6 +6,7 @@ data_bag_path    "data_bags"
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
+ssl_verify_mode :verify_peer
 
 #http_proxy  "http://172.16.2.9:80"
 #https_proxy "http://172.16.2.9:80"
@@ -15,14 +16,14 @@ knife[:berkshelf_path] = "cookbooks"
 #https_proxy "http://172.16.100.151:3128"
 #no_proxy    "192.168.*"
 
-if ENV["http_proxy"]
-  require 'rest-client'
-  RestClient.proxy = ENV["http_proxy"]
-
-  require 'uri'
-  proxy_env = URI.parse(ENV["http_proxy"])
-
-  http_proxy "http://#{proxy_env.host}:#{proxy_env.port}"
-  https_proxy "http://#{proxy_env.host}:#{proxy_env.port}"
-  no_proxy "192.168.*"
-end
+#if ENV["http_proxy"]
+#  require 'rest-client'
+#  RestClient.proxy = ENV["http_proxy"]
+#
+#  require 'uri'
+#  proxy_env = URI.parse(ENV["http_proxy"])
+#
+#  http_proxy "http://#{proxy_env.host}:#{proxy_env.port}"
+#  https_proxy "http://#{proxy_env.host}:#{proxy_env.port}"
+#  no_proxy "192.168.*"
+#end
