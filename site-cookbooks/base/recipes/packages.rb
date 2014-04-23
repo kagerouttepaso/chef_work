f2d6643e76ce561117088892fc12ca6687009510
kitsunai <goth.wasawasa@gmail.com>
2014-04-23 21:37:16 +0900

    add docker test

--- site-cookbooks/base/recipes/packages.rb
+++ site-cookbooks/base/recipes/packages.rb
@@ -29,3 +29,26 @@ simple_iptables_rule "mosh" do
   rule "--proto udp --dport 60000:61000"
   jump "ACCEPT"
 end
+
+if node['platform_version'].to_f >= 14.04  then
+  %w{docker.io}.each do |pkg|
+    package pkg do
+      action :upgrade
+    end
+  end
+  simple_iptables_rule "shipyard" do
+    rule "--proto tcp --dport 8000"
+    jump "ACCEPT"
+  end
+
+  home_dir="/home/"+node[:current_user]
+  execute "run docker ui" do
+    user    node[:current_user]
+    action  :nothing
+    cwd     home_dir
+    command "./install.sh"
+    environment(
+      "HOME" => home_dir
+    )
+  end
+end
