23a6d8a9db7838d81f355d1edb919165d5121fc2
kitsunai <goth.wasawasa@gmail.com>
2014-04-24 19:46:45 +0900

    delete

--- private-cookbooks/pxe-server/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'pxe-server'
-maintainer       'sanritz automation'
-maintainer_email 'kitsunai@sanritz.co.jp'
-license          'MIT'
-description      'Installs/Configures pxe-server'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
