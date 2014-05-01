0230147c56ef1207a315cf6ec45938401700ed57
kitsunai yuki <goth.wasawasa@gmail.com>
2014-05-01 19:18:54 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,8 +37,8 @@ if [ "`printenv | grep -i 'http_proxy'`" = "" ]; then
     fi
 else
     echo http_proxy is ${http_proxy}
-    if [ -f ~/.gitconfig ] && [ `cat ~/.gitconfig | grep -i proxy` != "" ] ; then
-        echo git proxy setting is enable
+    if [ -f ~/.gitconfig ] && [ "`cat ~/.gitconfig | grep -i proxy`" != "" ] ; then
+        ;
     else
         git config --global http.proxy "${PROXY}"
     fi
