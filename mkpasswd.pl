9095afdee188b1f2e4617a8e74de7ad404c471f7
kitsunai <kitsunai@sanritz.co.jp>
2014-05-19 10:02:19 +0900

    modify mkpasswd.pl

--- mkpasswd.pl
+++ mkpasswd.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
-print "password:";
-chomp($passwd = <STDIN>);
 print "solt:";
 chomp($solt = <STDIN>);
+print "password:";
+chomp($passwd = <STDIN>);
 $hash = crypt("$passwd", "\$6\$$solt");
 print "$hash";
 
